c3d8c5d5ee0b93b3f9131f1433190df5a234e806
k9227 <k9227@student.jamk.fi>
2017-10-25 10:43:17 +0300

    lab 8 assigment 1

---
+++ Labra8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //KÄYTTÄKÄÄ AINA TRY-CATCHIA
+            try
+            {
+                string nimi;
+                string filepath =Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string filu = filepath + @"\testi.txt";
+                //kirjoitetaa käyttäjän antamat rivit tiedosotoon
+                //luodaan StreamWriter tyyppinen olio johon kirjoitetaan
+                StreamWriter sw = new StreamWriter(filu);
+                do
+                {
+                    Console.Write("Anna nimi (Enter lopettaa): ");
+                    nimi = Console.ReadLine();
+                    if (nimi.Length > 0)
+                    {
+                        sw.WriteLine(nimi);
+                    }
+                } while (nimi.Length != 0);
+                sw.Close();
+                //avataan tiedosto uudestaan ja luetaan sen sisältö joka näytetään konsolissa
+                if (File.Exists(filu))
+                { 
+                    string teksti = File.ReadAllText(filu);
+                    Console.WriteLine(teksti);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);               
+            }
+        }
+    }
+}
