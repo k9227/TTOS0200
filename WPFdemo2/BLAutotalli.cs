10d48c04c3979727423359844c18a757fc2e99da
k9227 <k9227@student.jamk.fi>
2017-11-15 10:48:50 +0200

    added wpfdemo2

---
+++ WPFdemo2/BLAutotalli.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFdemo2
+{
+    public static class Autotalli
+    {
+        public static List<Auto> HaeAutot()
+        {
+            List<Auto> autot = new List<Auto>();
+            //GUIn testaamista varten keksitään muutama auto
+            Auto a = new Auto();
+            a.Merkki = "Volvo";
+            a.Malli = "V70";
+            a.VM = 2007;
+            a.KM = 200000;
+            a.Hinta = 9000F;
+            a.URL = "VolvoV70.png";
+            autot.Add(a);
+            //TOinen auto
+            Auto b = new Auto();
+            b.Merkki = "Audi";
+            b.Malli = "A4";
+            b.VM = 2008;
+            b.KM = 345000;
+            b.Hinta = 11000F;
+            b.URL = "AudiA4.png";
+            autot.Add(b);
+            Auto c = new Auto();
+            c.Merkki = "Saab";
+            c.Malli = "99";
+            c.VM = 1998;
+            c.KM = 750000;
+            c.Hinta = 1000F;
+            c.URL = "";
+            autot.Add(c);
+            Auto d = new Auto();
+            d.Merkki = "Toyota";
+            d.Malli = "Corolla";
+            d.VM = 1997;
+            d.KM = 175000;
+            d.Hinta = 1500F;
+            d.URL = "Corolla.jpg";
+            autot.Add(d);
+            //palautus
+            return autot;
+        }
+    }
+    public class Auto
+    {
+        public string Merkki { get; set; }
+        public string Malli { get; set; }
+        public int VM { get; set; }
+        public int KM { get; set; }
+        public float Hinta { get; set; }
+        public string URL { get; set; }
+    }
+}
---
+++ WPFdemo2/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFdemo2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Auto> autot; //muuttujat on käytettävisää kaikissa luokan metoideissa ja tapahtumankäsittelijöissä
+        private const string polku = @"D:\\k9227\\kuveja\\";
+        public MainWindow()
+        {
+            //tänne koodi joka suoritetaan ikkunan avauksen luonnissa
+            InitializeComponent();
+            NaytaKuva("autotalli.png");
+            //pyydetään BL-kerrokselta autot, ja näytetään ne käyttäjälle
+            autot = Autotalli.HaeAutot();
+        }
+
+        private void btnHaeAutot_Click(object sender, RoutedEventArgs e)
+        {    
+            dgAutot.ItemsSource = autot;            
+        }
+        private void NaytaKuva(string url)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(url))
+                {
+                    url = "puuttuu.png";
+                }
+                //lisätään kuvatiedostojen vakiopolku
+                url = polku + url;
+                //kuvan näyttö
+                BitmapImage pic = new BitmapImage();
+                pic.BeginInit();
+                pic.UriSource = new Uri(url);
+                pic.EndInit();
+                imgAutot.Stretch = Stretch.Fill;
+                imgAutot.Source = pic;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }
+        }
+
+        private void dgAutot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //huom, olemme itse populoineet DataGridin Auto-olioilla, joten kukin DataGridin jäsen/alkio on Auto-olio
+            object obj = dgAutot.SelectedItem;
+            if(obj != null)
+            {
+                Auto valittu = (Auto)obj;
+                NaytaKuva(valittu.URL);
+            }
+        }
+
+        private void btnHaeAudit_Click(object sender, RoutedEventArgs e)
+        {
+            //hakee vain AUdit
+            var result = autot.Where(m => m.Merkki.Contains("Audi"));
+            dgAutot.ItemsSource = result;
+        }
+    }
+}
